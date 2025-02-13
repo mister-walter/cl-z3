25eefe39a0e6582578203714051289e722516a80
Andrew Walter <me@atwalter.com>
2025-02-13 12:02:00 -0500

    Replace sort -> z3-sort in func-decl
    This was an oversight when the sort type was renamed to z3-sort.

--- src/z3/func-decl.lisp
+++ src/z3/func-decl.lisp
@@ -5,12 +5,12 @@
 (defun func-decl-domain (decl context)
   "Get the domain of the function declaration, as a list of sorts."
   (loop for i below (z3-get-domain-size context decl)
-        collect (make-instance 'sort
+        collect (make-instance 'z3-sort
                                :handle (z3-get-domain context decl i)
                                :context context)))
 
 (defun func-decl-range (decl context)
   "Get the range of the function declaration, as a sort."
-  (make-instance 'sort
+  (make-instance 'z3-sort
                  :handle (z3-get-range context decl)
                  :context context))
